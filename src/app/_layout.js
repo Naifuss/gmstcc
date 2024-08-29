2fd3d8e5a2089193533e08bfc51bbaff4686eeb6
Gabriel Henrique <124268998+Naifuss@users.noreply.github.com>
2024-08-29 12:09:36 +0000

    first-commit

---
+++ src/app/_layout.js
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import { AppProvider } from "../hooks";
+
+export default function layout() {
+  return (
+    <AppProvider>
+      <Stack />
+    </AppProvider>
+  );
+}

2f96b59bc044f9994baf76d1d12de92be9433a5e
Gabriel Henrique <124268998+Naifuss@users.noreply.github.com>
2024-09-05 12:38:32 +0000

    [feat] Configurando_Hook

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
-import {useAuth} from "../hooks/Auth"
+import { useAuth } from "../hooks/Auth";
 
 export default function App() {
-  const { signIn, signOut} = useAuth();
+  const { signIn, signOut } = useAuth();
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Aplicativo pronto para usar</Text>
-      <Button title="Signin"/>
-      <Button title="Signout" onPress={() => signOut()}/>
-      <StatusBar style="auto" />
+      <Button
+        title="Signin Super"
+        onPress={() =>
+          signIn({ email: "super@gmail.com", password: "Super123!" })
+        }
+      />
+      <Button
+        title="Signin Adm"
+        onPress={() => signIn({ email: "adm@gmail.com", password: "Adm123!" })}
+      />
+      <Button
+        title="Signin User"
+        onPress={() =>
+          signIn({ email: "user@gmail.com", password: "User123!" })
+        }
+      />
+      <Button title="SignOut" onPress={() => signOut()} />
+      <StatusBar style="auto" /> 
     </View>
   );
 }

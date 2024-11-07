ee8163bcbf8c26a674a6809eb7f3ac6c75d21f3f
Gabriel Henrique <124268998+Naifuss@users.noreply.github.com>
2024-11-07 12:49:31 +0000

    [Feat] meio-SQLite

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,8 +1,11 @@
 import { AuthProvider } from "./Auth";
+import { DataProvider } from "./Data";
 import { FontProvider } from "./Font";
 
 export function AppProvider({ children}) {
     return <FontProvider>
-        <AuthProvider>{children}</AuthProvider>
+        <DataProvider>
+            <AuthProvider>{children}</AuthProvider>
+        </DataProvider>
     </FontProvider>;
 }
